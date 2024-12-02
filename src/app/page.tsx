077b54fdb83075bd4285e378054a52d9134dfebf
Jerit Baiju <jeritalumkal@gmail.com>
2024-12-02 18:54:53 +0530

    feat: add LiveLogs component to display real-time logs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 import Widgets from '@/components/Dashboard/Widgets';
 import Wrapper from './Wrapper';
+import LiveLogs from '@/components/Dashboard/LiveLogs';
 
 export default function Home() {
   return (
     <Wrapper>
       <Widgets />
+      <LiveLogs />
     </Wrapper>
   );
 }
